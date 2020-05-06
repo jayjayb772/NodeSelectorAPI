4a8ac6b88da96a19ef256ac0e163c599731dc5dc
Jacob <jayjayb100@msn.com>
2020-05-06 14:56:15 -0500

    reformated returned html

--- routes/formatQuery.js
+++ routes/formatQuery.js
@@ -20,14 +20,14 @@ exports.formatMultipleElements = function(queries, res){
 
 function buildHtml(query, addr){
     let htmlDoc = '<!DOCTYPE html>'
-    + '<html><div style="text-align: center"><h3>';
+    + '<html><div style="text-align: center"><h5>';
     htmlDoc+=`Your Address: ${addr}`;
-    htmlDoc+= '</h3><br><body>';
+    htmlDoc+= '</h5><br><h3><body>';
     htmlDoc+= `Closest Node: ${query["NEIGHBORHOOD_ID"]}<br>`
         + `Node Name: ${query["NODE_NAME"]}<br>`
         + `Primary Contact: ${query["PRIMARY_CONTACT"]}<br>`
         + `Phone number: ${query["PHONE"]}<br>`
         + `Email:<a href="mailto:${query["EMAIL"]}">${query["EMAIL"]}</a><br>`
-        +'</body></div></html>';
+        +'</body></h3></div></html>';
     return htmlDoc;
 }

7ad932a09888d7ad5552ca478a340dbfbdc240c0
Jacob <jayjayb100@msn.com>
2020-05-06 12:22:37 -0500

    put html doc in response to closest node

--- routes/formatQuery.js
+++ routes/formatQuery.js
@@ -20,9 +20,9 @@ exports.formatMultipleElements = function(queries, res){
 
 function buildHtml(query, addr){
     let htmlDoc = '<!DOCTYPE html>'
-    + '<html><h1>';
+    + '<html><h6>';
     htmlDoc+=`Your Address: ${addr}`;
-    htmlDoc+= '<h1><br><body>';
+    htmlDoc+= '</h6><br><body>';
     htmlDoc+= `Closest Node: ${query["NEIGHBORHOOD_ID"]}<br>`
         + `Node Name: ${query["NODE_NAME"]}<br>`
         + `Primary Contact: ${query["PRIMARY_CONTACT"]}<br>`
